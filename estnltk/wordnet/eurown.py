db19ac633b6f18e458d2d0a4087496915e5a39b9
Neeme Kahusk <nemee@ut.ee>
2018-03-22 17:49:02 +0200

    wnwb query

--- estnltk/wordnet/eurown.py
+++ estnltk/wordnet/eurown.py
@@ -51,7 +51,33 @@ start_time = time.time()
 from lxml import etree
 from lxml.etree import fromstring, tostring
 
-from wnconfig import SYNSET_RELATIONS
+import sys
+import requests
+
+from wnconfig import WNWB_PREFIX, WNWB_LEXID, HYPERNYM_IDS, HYPONYM_IDS, APP_CONFIG_SECRET_KEY, SYNSET_RELATIONS
+
+
+def make_wnwb_query(lexid, search, root):
+    """
+    Make wnwb query 
+    """
+    tulem = []
+    r = requests.get(root, params={
+        'lexid':lexid, 'word':search}
+                         )
+    print(r.url)
+    if r.status_code != 200:
+        print ('Ei saa serveriga ühendust!', sys.stderr)
+        return None
+    else:
+        vastus = r.json()
+        tulem = vastus['results']
+        if vastus['count'] > len(vastus['results']):
+            r = requests.get(vastus['next'])
+            if r.status_code == 200:
+                tulem.extend(r.json()['results'])
+        return tulem
+
 
 
 def _pos_from_number(iStr):
