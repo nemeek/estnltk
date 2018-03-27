9693d1f57373e81b07e55d9c996bd1d9ca74758d
Neeme Kahusk <nemee@ut.ee>
2018-03-27 18:21:44 +0300

    lxml snippets

--- estnltk/wordnet/t_synsets.py
+++ estnltk/wordnet/t_synsets.py
@@ -2,6 +2,11 @@
 
 """Test eurown module
 
+https://docs.python.org/3/library/profile.html
+python -m cProfile [-o output_file] [-s sort_order] myscript.py
+
+http://lxml.de/parsing.html#iterparse-and-iterwalk
+
 """
 
 # Timer
